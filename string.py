326df704b31c4626466ca4b25f4feac427829434
SR-Rony <srrony707@gmail.com>
2025-04-29 00:03:32 -0400

    python string add

---
+++ string.py
@@ -0,0 +1,12 @@
+str = "i love pytho"
+index = str[0]
+
+#slice string print
+slice = str[2:len(str)-1]
+#string end function
+last= str.endswith("o")
+
+#string capitalize function
+capitalize = str.capitalize()
+print(capitalize)
+
