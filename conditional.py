4580d9f6b2a68b19bc4312e261176671be5ed1a1
SR-Rony <srrony707@gmail.com>
2025-04-29 00:49:25 -0400

    conditional statement add

---
+++ conditional.py
@@ -0,0 +1,25 @@
+#all conditional statements add
+# number = 19
+# if (number >18):
+#     print("You are eligible to vote in year 18+")
+# elif (number == 18):
+#     print("You are eligible to vote")
+# else:
+#     print("You are not eligible to vote")
+
+number= float(input("Enter a number:"))
+
+if(number >= 90):
+    print("A+")
+elif(number >= 80):
+      print("A")
+elif(number >= 70):
+    print("B")
+elif(number >= 60):
+    print("C")
+elif(number >= 50):
+    print("D")
+elif(number >= 40):
+    print("E")
+else:
+    print("F")
