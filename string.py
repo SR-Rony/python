d75ad9ff410752c6d726eaade8c2f04965a68496
SR-Rony <srrony707@gmail.com>
2025-04-29 00:21:37 -0400

    string function apply

--- string.py
+++ string.py
@@ -1,4 +1,4 @@
-str = "i love pytho"
+str = "i love python love programming"
 index = str[0]
 
 #slice string print
@@ -8,5 +8,14 @@ last= str.endswith("o")
 
 #string capitalize function
 capitalize = str.capitalize()
-print(capitalize)
+
+#replace string function
+str = str.replace("python","javascript")
+
+#string find function
+find = str.find("e")
+#string count function
+count = str.count("love")
+
+print(count)
 
