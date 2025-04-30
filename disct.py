5910ae46ccaccfe661875d43e2f474480c195294
SR-Rony <srrony707@gmail.com>
2025-04-30 01:33:50 -0400

    pyton disct add

---
+++ disct.py
@@ -0,0 +1,18 @@
+student ={
+    "name" : "SR",
+    "age" : 20,
+    "address" : {
+        "city" : "Delhi",
+        "state" : "Delhi",
+        "country" : "India"
+    },
+    "hobbies" : ["reading", "writing", "coding"],
+    "marks" : {
+        "maths" : 90,
+        "english" : 85,
+        "science" : 95
+    },
+    "is_student" : True,
+}
+
+print(student.keys())
