ca5bf9241216c0bb0e96d85bf6b7338e4488193f
SR-Rony <srrony707@gmail.com>
2025-05-01 00:15:58 -0400

    python loop add

--- loop.py
+++ loop.py
@@ -1,4 +1,18 @@
 #python loop
+
+country = ["USA", "UK", "Germany", "France", "Italy"]
+for i in range(len(country)):
+    print(country[i])
+
+
+
+
+
+
+
+
+
+
 # count = 0
 # while count <= 10:
 #     print("hello",count)
@@ -12,10 +26,43 @@
 #     print(n,"x",n,"=",(n*n))
 #     n += 1
 
-number = [33,22,55,66,88,99,88,6,0,6,5,43,]
+number = [2,3,5,7,8,9,11]
 
-num = 0
-while 1 < len(number):
-    print(number[num])
-    num += 1
-print("Loop finished")
+
+# even = []
+# i= 0
+# while i < len(number):
+#     if number[i]%2 ==0:
+#         even.append(number[i])
+#         print("even number",number[i])
+#     else:
+#         print("odd number",number[i])
+    # i += 1
+# result = 0
+
+# i = 0
+
+# while i < len(number):
+#     result += number[i]
+#     print("it is a big number",result)
+#     i+= 1
+
+
+
+
+
+# x  = 0
+# i = 0 
+# while i < len(number):
+
+#     if x>number[i]:
+#         x = number[i]
+#         # print("Max number is",x)
+#     else:
+#         # print("Not found")
+
+# num = 0
+# while 1 < len(number):
+#     print(number[num])
+#     num += 1
+# print("Loop finished")
