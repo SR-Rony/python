947ea4ecd6423981cecbdc2cec6ebaf211a6e1b6
SR-Rony <srrony707@gmail.com>
2025-04-30 20:52:51 -0400

    python loop add

---
+++ loop.py
@@ -0,0 +1,21 @@
+#python loop
+# count = 0
+# while count <= 10:
+#     print("hello",count)
+#     count += 1
+# print("Loop finished")
+
+
+
+# n= 1
+# while n<= 10:
+#     print(n,"x",n,"=",(n*n))
+#     n += 1
+
+number = [33,22,55,66,88,99,88,6,0,6,5,43,]
+
+num = 0
+while 1 < len(number):
+    print(number[num])
+    num += 1
+print("Loop finished")
---
+++ set.py
@@ -0,0 +1,5 @@
+collection = {1,2,3,4,6,7,8,9,"world"}
+collection.add(5)
+result =collection.pop()
+print(result)
+print(collection)
