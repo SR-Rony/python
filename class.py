17799bd601e51a93f5a3abbe9fc762b474d202b6
SR-Rony <srrony707@gmail.com>
2025-05-03 12:02:24 +0600

    account program add

--- class.py
+++ class.py
@@ -1,18 +1,55 @@
-class Student :
-    def __init__(self,name,age,grade):
+
+class account :
+    def __init__(self,name,blance,pin):
         self.name = name
-        self.age = age
-        self.grade = grade
+        self.balnce = blance
+        self.pin = pin
 
-    def hello(self):
-        print("Hello",self.name ,self.age)
+    def deposit(self,amount):
+        print("Deposit amount is : ",amount)
+        self.balnce +=amount
+        print("Deposit is successfull")
+        print("Your new balance is : ",self.balnce)
 
-    def ages(self):
-        print("Hello",self.name ,self.age)
-        return self.age
+    def withdraw(self,amount):
+        print("Withdraw ammount is : ",amount)
+        self.balnce -= amount
+        print("Withdraw is successfull")
+        print("Your new balance is : ",self.balnce)
+    def check_balance(self):
+        print("Your current balance is : ",self.balnce)
+
+
+coustomar1 = account("SR-Rony",100,1234)
+
+print("coustomar name is : ",coustomar1.name)
+print("coustomar balance is : ",coustomar1.balnce)
+print("coustomar pin is : ",coustomar1.pin)
+coustomar1.deposit(900)
+coustomar1.withdraw(300)
+coustomar1.check_balance()
 
 
 
-s1 = Student("SR",25,"A+")
-# print(s1.hello(name))
-# print(s1.ages(22))
+
+
+
+
+# class Student :
+#     def __init__(self,name,marks,):
+#         self.name = name
+#         self.marks = marks
+
+#     def avaerage(self):
+#         sum = 0
+#         for val in self.marks:
+#             sum += val
+#         result =  sum/3
+
+#         print("Average of marks is : ",result)
+
+
+
+# s1 = Student("SR",[60,90,50])
+
+# s1.avaerage()
