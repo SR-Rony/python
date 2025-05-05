742b303ae26f97504b18a1c0c37be3f1961b7650
SR-Rony <srrony707@gmail.com>
2025-05-05 22:25:46 +0600

    python string formate add

---
+++ map.py
@@ -0,0 +1,13 @@
+# python map function
+
+def my_fun(num):
+
+    if num % 2 == 1:
+        return num +1
+    else:
+        return num
+    
+
+list = [11,22,33,55,66,77,88,89,6,4543,33,]
+
+print (my_fun(list))
---
+++ string_1.py
@@ -0,0 +1,24 @@
+string = "Hello world"
+
+# string slice
+slice = string[0:3]
+
+# string character
+character = string[2]
+
+
+# string loop
+ 
+# for x in string:
+#     # print(x)
+
+splite = string.split(" ")
+print(splite)
+# replace = string.replace("Hello","Welcome")
+# print(replace)
+print(string)
+
+# print(string.strip())
+# print(string.upper())
+# print(string.lower())
+
