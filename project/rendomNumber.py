69040d5bd410ad19e91a2801f9af61b68d719b8d
SR-Rony <srrony707@gmail.com>
2025-05-07 16:33:28 +0600

    number guessed games add

--- project/rendomNumber.py
+++ project/rendomNumber.py
@@ -1,15 +1,35 @@
 import random
 
+print("welcome to number gussing games")
+
+guessNumber = random.randint(1,100)
+
+
 while True :
+    try:
+        userNumber = int(input("Please give tha choice your number: "))
+        
+        if guessNumber < userNumber :
+            print("to high number")
+        elif guessNumber > userNumber:
+            print("to low number")
+        else :
+            print("you guessed tha number",guessNumber)
+            break
 
-    choice = input("Roll the dice? Y/N : ").lower()
+    except ValueError :
+        print("please inter a valid number")
 
-    if choice == "y":
-        die1 = random.randint(1,6)
-        die2 = random.randint(1,6)
-        print(f"({die1},{die2})")
-    elif choice == "n":
-        print("Thank you for plaing!")
-        break
-    else:
-        print("Invelid choice")
+# while True :
+
+#     choice = input("Roll the dice? Y/N : ").lower()
+
+#     if choice == "y":
+#         die1 = random.randint(1,6)
+#         die2 = random.randint(1,6)
+#         print(f"({die1},{die2})")
+#     elif choice == "n":
+#         print("Thank you for plaing!")
+#         break
+#     else:
+#         print("Invelid choice")
