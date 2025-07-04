f66b022e1ed37d5ac4386b20ca996dba68cbf5b6
SR-Rony <srrony707@gmail.com>
2025-07-04 08:45:08 +0600

    new class type add

---
+++ new_class/type.py
@@ -0,0 +1,16 @@
+str = "welcome to python world"  #this is a string type
+num = 100 #this is an integer type
+flt = 10.5 #this is a float type
+lst = [1, 2, 3, 4, 5] #this is a list type
+tup = (1, 2, 3, 4, 5) #this is a tuple type
+dic = {"name": "John", "age": 30} #this is a dictionary type
+set_ = {1, 2, 3, 4, 5} #this is a set type
+
+range_ = range(5) #this is a range type
+
+print(range_)  # <class 'str'>
+com = 1 + 2j # this is a complex number type
+
+bool = True # this is a boolean type
+
+print(type(com))  # <class 'str'>
